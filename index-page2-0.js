9d34bc09665351bcdda6992584ade9972f32d1b5
yerkebulan.omar <yerkebulan.omar@gmail.com>
2023-01-29 04:26:04 +0600

    fetched second page

--- index-page2-0.js
+++ index-page2-0.js
@@ -1,37 +1,86 @@
-const content = [
-  {
-    id: 0,
-    avatarImg: "/assets/Ryan Holiday.png",
-    authorName: "Ryan Holiday",
-    date: "Jan 12",
-    readingTime: "9 min read",
-    title: "If You Only Read A Few Books In 2023, Read These",
-    summary:
-      "It’d be wonderful if a new year magically marked a new beginning. But 2022, like all years, reminded us that the same things keep happening, that world events continue on in their own unpredictable way and that in the end, we control very little but our own actions and opinions.",
-    articleImg: "/assets/books article.jpg",
-    subheader:
-      "12 books that will help you meet the goals that matter for 2023",
-    text: `One of my favorite quotes — enough that I have it inscribed on the wall across the back of my bookstore — comes from the novelist Walter Mosley. “I’m not saying that you have to be a reader to save your soul in the modern world,” he said. “I’m saying it helps.”
-            2023 stands before us promising nothing but the same difficulties and opportunities that last year and every year before it promised. Maybe even new and worse ones. What are you going to do about it? Will you be ready for it? Can you handle it?<br>
-          <br>
-          Books are an investment in yourself — investments that come in many forms: novels, nonfiction, how-to, poetry, classics, biographies. They help you think more clearly, be kinder, see the bigger picture, and improve at the things that matter to you. Books are a tradition that stretches back thousands of years and stretches forward to today, where people are still publishing distillations of countless hours of hard thinking on hard topics. Why wouldn’t you avail yourself of this wisdom?<br>
-          <br>
-          With that in mind, here are 12 books — some new, some old — that will help you meet the goals that matter for 2023, that will help you live better and be better.`,
-    likes: 230,
-    comments: 34,
-  },
-];
+// const content = [
+//   {
+//     id: 0,
+//     avatarImg: "/assets/Ryan Holiday.png",
+//     authorName: "Ryan Holiday",
+//     date: "Jan 12",
+//     readingTime: "9 min read",
+//     title: "If You Only Read A Few Books In 2023, Read These",
+//     summary:
+//       "It’d be wonderful if a new year magically marked a new beginning. But 2022, like all years, reminded us that the same things keep happening, that world events continue on in their own unpredictable way and that in the end, we control very little but our own actions and opinions.",
+//     articleImg: "/assets/books article.jpg",
+//     subheader:
+//       "12 books that will help you meet the goals that matter for 2023",
+//     text: `One of my favorite quotes — enough that I have it inscribed on the wall across the back of my bookstore — comes from the novelist Walter Mosley. “I’m not saying that you have to be a reader to save your soul in the modern world,” he said. “I’m saying it helps.”
+//             2023 stands before us promising nothing but the same difficulties and opportunities that last year and every year before it promised. Maybe even new and worse ones. What are you going to do about it? Will you be ready for it? Can you handle it?<br>
+//           <br>
+//           Books are an investment in yourself — investments that come in many forms: novels, nonfiction, how-to, poetry, classics, biographies. They help you think more clearly, be kinder, see the bigger picture, and improve at the things that matter to you. Books are a tradition that stretches back thousands of years and stretches forward to today, where people are still publishing distillations of countless hours of hard thinking on hard topics. Why wouldn’t you avail yourself of this wisdom?<br>
+//           <br>
+//           With that in mind, here are 12 books — some new, some old — that will help you meet the goals that matter for 2023, that will help you live better and be better.`,
+//     likes: 230,
+//     comments: 34,
+//   },
+// ];
+
+// content.forEach((item) => {
+//   let newArticle = code.replace(
+//     `id="imgAva" src="`,
+//     `id="imgAva" src="${item.avatarImg}"`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="author" class="authors-name">`,
+//     `id="author" class="authors-name">${item.authorName}`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="date" class="grey-text date">`,
+//     `id="date" class="grey-text date">${item.date}`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="readingTime" class="grey-text time-spent">`,
+//     `id="readingTime" class="grey-text time-spent">${item.readingTime}`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="title" class="title">`,
+//     `id="title" class="title">${item.title}`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="summary" class="summary">`,
+//     `id="summary" class="summary">${item.summary}`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="imagePage" src=""`,
+//     `id="imagePage" src="${item.articleImg}"`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="subheader" class="subheader">`,
+//     `id="subheader" class="subheader">${item.subheader}`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="mainText" class="main-text">`,
+//     `id="mainText" class="main-text">${item.text}`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="likes" class="likes-number grey-text">`,
+//     `id="likes" class="likes-number grey-text">${item.likes}`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="comment" class="comment-number grey-text">`,
+//     `id="comment" class="comment-number grey-text">${item.comments}`
+//   );
+
+//   article.innerHTML += newArticle;
+// });
 
 const code = `<div class="news-page">
 <div class="top-part">
   <div class="top-left">
-    <img id="imgAva" src="" alt="avatar" />
+    <img id="imgAva" src="/assets/author.jpg" alt="avatar" />
     <div class="text-info">
       <h3 id="author" class="authors-name"></h3>
       <div class="text-info-grey">
         <h5 id="date" class="grey-text date"></h5>
         <h5 class="grey-text">&nbsp·&nbsp</h5>
-        <h5 id="readingTime" class="grey-text time-spent"></h5>
+        <h5 id="readingTime" class="grey-text time-spent">5 min read</h5>
         <h5 class="grey-text">&nbsp·&nbsp</h5>
         <h5 class="grey-text for-whom">Member-only</h5>
       </div>
@@ -55,15 +104,20 @@ const code = `<div class="news-page">
 </div>
 <img id="imagePage" src="" class="page-image" alt="laptop" />
 <div class="main">
-  <h4 id="subheader" class="subheader"></h4>
-  <p id="mainText" class="main-text"></p>
+  <h4 id="subheader" class="subheader">"12 books that will help you meet the goals that matter for 2023"</h4>
+  <p id="mainText" class="main-text">One of my favorite quotes — enough that I have it inscribed on the wall across the back of my bookstore — comes from the novelist Walter Mosley. “I’m not saying that you have to be a reader to save your soul in the modern world,” he said. “I’m saying it helps.”
+              2023 stands before us promising nothing but the same difficulties and opportunities that last year and every year before it promised. Maybe even new and worse ones. What are you going to do about it? Will you be ready for it? Can you handle it?<br>
+            <br>
+  Books are an investment in yourself — investments that come in many forms: novels, nonfiction, how-to, poetry, classics, biographies. They help you think more clearly, be kinder, see the bigger picture, and improve at the things that matter to you. Books are a tradition that stretches back thousands of years and stretches forward to today, where people are still publishing distillations of countless hours of hard thinking on hard topics. Why wouldn’t you avail yourself of this wisdom?<br>
+  <br>
+  With that in mind, here are 12 books — some new, some old — that will help you meet the goals that matter for 2023, that will help you live better and be better.</p>
 </div>
 <div class="bottom-part">
   <div class="likes-comments">
     <img src="/assets/Heart.png" alt="heart" />
-    <h5 id="likes" class="likes-number grey-text"></h5>
+    <h5 id="likes" class="likes-number grey-text">123</h5>
     <img src="/assets/Speech Bubble.png" alt="speech" />
-    <h5 id="comment" class="comment-number grey-text"></h5>
+    <h5 id="comment" class="comment-number grey-text">12</h5>
   </div>
   <img class="bookmark" src="/assets/Bookmark.png" alt="bookmark" />
 </div>
@@ -71,51 +125,41 @@ const code = `<div class="news-page">
 
 const article = document.getElementById("article");
 
-content.forEach((item) => {
-  let newArticle = code.replace(
-    `id="imgAva" src="`,
-    `id="imgAva" src="${item.avatarImg}"`
-  );
-  newArticle = newArticle.replace(
-    `id="author" class="authors-name">`,
-    `id="author" class="authors-name">${item.authorName}`
-  );
-  newArticle = newArticle.replace(
-    `id="date" class="grey-text date">`,
-    `id="date" class="grey-text date">${item.date}`
-  );
-  newArticle = newArticle.replace(
-    `id="readingTime" class="grey-text time-spent">`,
-    `id="readingTime" class="grey-text time-spent">${item.readingTime}`
-  );
-  newArticle = newArticle.replace(
-    `id="title" class="title">`,
-    `id="title" class="title">${item.title}`
-  );
-  newArticle = newArticle.replace(
-    `id="summary" class="summary">`,
-    `id="summary" class="summary">${item.summary}`
-  );
-  newArticle = newArticle.replace(
-    `id="imagePage" src=""`,
-    `id="imagePage" src="${item.articleImg}"`
-  );
-  newArticle = newArticle.replace(
-    `id="subheader" class="subheader">`,
-    `id="subheader" class="subheader">${item.subheader}`
-  );
-  newArticle = newArticle.replace(
-    `id="mainText" class="main-text">`,
-    `id="mainText" class="main-text">${item.text}`
-  );
-  newArticle = newArticle.replace(
-    `id="likes" class="likes-number grey-text">`,
-    `id="likes" class="likes-number grey-text">${item.likes}`
-  );
-  newArticle = newArticle.replace(
-    `id="comment" class="comment-number grey-text">`,
-    `id="comment" class="comment-number grey-text">${item.comments}`
-  );
+const loadData = async () => {
+  try {
+    const url =
+      "https://api.nytimes.com/svc/topstories/v2/world.json?api-key=usqYURJ7MmPm8ike76kWVwAUH5f2TtWq";
+    const res = await fetch(url);
+    console.log(res.ok);
+    const data = await res.json();
+    console.log(data.results.splice(0, 1));
+    data.results.splice(0, 1).forEach((item) => {
+      let newArticle = code.replace(
+        `id="author" class="authors-name">`,
+        `id="author" class="authors-name">${item.byline}`
+      );
+      newArticle = newArticle.replace(
+        `id="date" class="grey-text date">`,
+        `id="date" class="grey-text date">${item.published_date.substr(0, 10)}`
+      );
+      newArticle = newArticle.replace(
+        `id="title" class="title">`,
+        `id="title" class="title">${item.title}`
+      );
+      newArticle = newArticle.replace(
+        `id="summary" class="summary">`,
+        `id="summary" class="summary">${item.abstract}`
+      );
+      newArticle = newArticle.replace(
+        `id="imagePage" src=""`,
+        `id="imagePage" src="${item.multimedia[0].url}"`
+      );
 
-  article.innerHTML += newArticle;
-});
+      article.innerHTML += newArticle;
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+loadData();
--- index.js
+++ index.js
@@ -1,7 +1,106 @@
+// const articlesFromAPI = [
+//   {
+//     id: 0,
+//     avatar: "/assets/Ryan Holiday.png",
+//     author: "Ryan Holiday",
+//     topic: "Better humans",
+//     published_date: "Jan 12",
+//     title: "If You Only Read A Few Books In 2023, Read These",
+//     abstract:
+//       "It’d be wonderful if a new year magically marked a new beginning. But 2022, like all years, reminded us that the same things keep happening, that world events continue on in their own unpredictable way and that...",
+//     section: "Stoicism",
+//     readingTime: "9 min read",
+//     textImage: "/assets/books article.jpg",
+//     url: "/page2-0.html",
+//     subheader:
+//       "12 books that will help you meet the goals that matter for 2023",
+//     likes: 230,
+//     comments: 34,
+//   },
+//   {
+//     id: 1,
+//     avatar: "/assets/Samer Buna.jpeg",
+//     author: "Samer Buna",
+//     topic: "EdgeCoders",
+//     published_date: "Jan 03",
+//     title: "The Mistakes I Made As a Beginner Programmer",
+//     abstract:
+//       "Learn to identify them, make habits to avoid them — Update: This article is now part of my book “The Professional Programmer”. Read the updated version of this content, and more programming advice at...",
+//     section: "IT",
+//     readingTime: "29 min read",
+//     textImage: "/assets/programming.jpeg",
+//     url: "/page2-1.html",
+//     subheader: "Think. Research. Plan. Write. Validate. Modify.",
+//     likes: 213,
+//     comments: 56,
+//   },
+//   {
+//     id: 2,
+//     avatar: "/assets/Aytekin Tank.jpeg",
+//     author: "Aytekin Tank",
+//     topic: "TheStartup",
+//     published_date: "Dec 28",
+//     title: "The power of doing nothing at all",
+//     abstract:
+//       "The old crocodile was floating at the river’s edge when a younger crocodile swam up next to him, “I’ve heard from many that you’re the fiercest hunter in all of the river bottoms. Please, teach me your ways....",
+//     section: "Business",
+//     readingTIme: "7 min read",
+//     textImage: "/assets/do nothing.jpeg",
+//     url: "/page2-2.html",
+//     subheader: "Doing what matters vs. busy-bragging",
+//     likes: 234,
+//     comments: 123,
+//   },
+// ];
+
+// articlesFromAPI.forEach((item) => {
+//   let newArticle = article.replace(
+//     `id="avatar" src=""`,
+//     `id="avatar" src="${item.avatar}"`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="authors-name">`,
+//     `id="authors-name">${item.author}`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="topics name">`,
+//     `id="topics name">${item.topic}`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="published_date" class="grey-text">`,
+//     `id="published_date" class="grey-text">${item.published_date}`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="article-name">`,
+//     `id="article-name">${item.title}`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="article-abstract">`,
+//     `id="article-abstract">${item.abstract}`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="button-text">`,
+//     `id="button-text">${item.section}`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="readingTime" class="grey-text">`,
+//     `id="readingTime" class="grey-text">${item.readingTime}`
+//   );
+//   newArticle = newArticle.replace(
+//     `id="textImage" src=""`,
+//     `id="textImage" src="${item.textImage}"`
+//   );
+//   newArticle = newArticle.replace(`url=""`, `url="${item.url}"`);
+
+//   newArticle = newArticle.replace(`url=""><img`, `url="${item.url}"><img`);
+
+//   articles.innerHTML += newArticle;
+// });
+
 const article = `<div class="news">
 <div class="left">
   <div class="top">
-    <img id="avatar" src="/assets/istockphoto-1220827245-612x612.jpg" class="img-ava" alt="avatar1" />
+    <img id="avatar" src="/assets/author.jpg" class="img-ava" alt="avatar1" />
     <h5 id="authors-name"></h5>
     <h5 class="grey-text">in</h5>
     <h5 id="topics name"></h5>
@@ -9,7 +108,7 @@ const article = `<div class="news">
     <h5 id="published_date" class="grey-text"></h5>
   </div>
   <div class="text">
-    <a href=""><h2 id="article-name"></h2></a>
+    <button id="article-name"><h2></h2></button>
     <p id="article-abstract"></p>
   </div>
   <div class="info-bottom">
@@ -46,12 +145,13 @@ const loadData = async () => {
     const res = await fetch(url);
     console.log(res.ok);
     const data = await res.json();
-    console.log(data.results);
+    console.log(data.results.splice(0, 5));
     data.results.splice(0, 5).forEach((item) => {
       let newArticle = article.replace(
         `id="authors-name">`,
         `id="authors-name">${item.byline}`
       );
+
       newArticle = newArticle.replace(
         `id="topics name">`,
         `id="topics name">${item.des_facet[0]}`
@@ -64,8 +164,8 @@ const loadData = async () => {
         )}`
       );
       newArticle = newArticle.replace(
-        `id="article-name">`,
-        `id="article-name">${item.title}`
+        `id="article-name"><h2>`,
+        `id="article-name"><h2>${item.title}`
       );
       newArticle = newArticle.replace(
         `id="article-abstract">`,
@@ -88,3 +188,9 @@ const loadData = async () => {
 };
 
 loadData();
+
+// const clickedArticle = document.querySelector("#article-name");
+
+// clickedArticle.addEventListener("click", function () {
+//   openInNewTab("http://www.test.com");
+// });
